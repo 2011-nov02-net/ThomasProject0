0f316736dbc733cb6d1d01f346e8430090982417
Thomas Judge <Judgemaster@gmail.com>
2020-11-07 20:53:57 -0500

    Restructuring Customers class based on new design model and inheritence structures which make a little more sense.

--- ThomasProjectZero.Library/Models/Customer.cs
+++ ThomasProjectZero.Library/Models/Customer.cs
@@ -13,16 +13,40 @@ namespace ThomasProjectZero.Library.Models
             get => _customerName;
             set
             {
-                if (value.Length == 0)
+                if (string.IsNullOrWhiteSpace(value))
                 {
-                    throw new ArgumentException("Reviewer name must not be empty.", nameof(value));
+                    //error checking
+                    throw new ArgumentException(message: "invalid name", paramName: nameof(value));
                 }
                 _customerName = value.
             } 
         }
-        var _customerName = _customerName.trim().toLowerCase();
+        //To prevent errors and so forth when searching to being capital sensitive
+        var _customerName = _customerName.trim().toUpperCase();
 
         public List<Order> Orders { get; set; } = new List<Order>();
+
+        //will need to read in information from the customer class object, 
+        //the Datetime class to produce a time stamp, and the current stock of a given product 
+        //at the target location of a given instance of WalbMart.
+        public void PlaceOrder(string customerFullName, string locationOfPurchase, string productName, int quanitityOfPurchase, double costOfPurchase, DateTime date, int walbMartCurrentStockOfProduct)
+        {
+            if (walbMartCurrentStockOfProduct >= quanitityOfPurchase && quanitityOfPurchase > 0)
+            {
+                Console.WriteLine("Customer: " + nameof(customerFullName));
+                Console.WriteLine("Has purchased:  x" + nameof(quantityOfPurchase) + " " nameof(productName));
+                Console.WriteLine("At WalbMart: " + nameof(locationOfPurchase)); 
+                Console.WriteLine("Date of Purchase: " + nameof(date));
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(nameof(quanitityOfPurchase), "You didn't buy anything");
+                break;
+            }
+            var order = new Order(customerFullName, locationOfPurchase, productName, quanitityOfPurchase, costOfPurchase, date);
+            Orders.Add(order);
+        }
+
 
     }
 }
